c325a1f980b2de3dd73e75bae9f2d70ebd023644
Adhilakshmi Mallem <Mallem_a@India.XoriantCorp.com>
2022-02-08 15:30:01 +0530

    build

---
+++ packer/setup.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+# Install logging agnet
+curl -sSO https://dl.google.com/cloudagents/add-logging-agent-repo.sh
+sudo bash add-logging-agent-repo.sh
+sudo apt-get update
+sudo apt-get install -y 'google-fluentd=1.*'
+
+# Install the Configuration files
+sudo mkdir -p /etc/google-fluentd/config.d
+
+# Start agent
